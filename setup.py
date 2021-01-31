031e6b1a5fd5e2edc01c9ad5c2c221590a704a14
DearCaat <33518172+DearCaat@users.noreply.github.com>
2021-01-31 07:14:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,28 +7,20 @@ from setuptools import setup,find_packages
 # Package meta-data.
 NAME = "ioplin"
 VERSION="0.0.1"
-DESCRIPTION = "IOPLIN model implementation. Keras and TensorFlow Keras."
-URL = "https://github.com/HuangSheng-CQU"
+DESCRIPTION = "IOPLIN model keras implementation, and simple application"
+URL = "https://github.com/DearCaat/ioplin"+
 EMAIL = "20171607@cqu.edu.cn"
 AUTHOR = "wenhao tang"
 REQUIRES_PYTHON = "<=3.7.9"
 
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
 here = os.path.abspath(os.path.dirname(__file__))
 
-# What packages are required for this module to be executed?
 try:
     with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as f:
         REQUIRED = f.read().split("\n")
 except:
     REQUIRED = []
 
-# Where the magic happens:
 setup(
     name=NAME,
     version=VERSION,
